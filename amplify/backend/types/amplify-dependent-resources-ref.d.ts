96f9393e3bd07fd75511686820de272a4693b406
nicolas <ngandalf2@gmail.com>
2023-11-15 14:44:45 +0100

    add storage S3 for images

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "amplifyappS3demo": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
